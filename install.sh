583a876225459b8f22f8725b97cc3f95f05a524f
Aurora Daniel <noahd29@parkschool.net>
2025-10-08 23:56:15 -0400

    Revert "Build waybar from scratch"
    This reverts commit 0c97b8a4f23a0edb73ee1bf254028867d3736850.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 cd ~
 # === Package installation ===
 
@@ -12,12 +13,12 @@ if ! paru -V; then
 fi
 
 # -- So many packages --
-paru -S --needed ark bluez bluez-utils brightnessctl btop catpuccin-gtk-theme-mocha cava fastfetch fcitx5 fcitx-chinese-addons fish gnome-bluetooth grim hyprcursor-dracula-kde-git hypridle hyprland hyprlock hyprpaper hyprpolkitagent hyprsunset kitty libinput-gestures ly mako nemo nautilus noto-fonts-cjk noto-fonts-emoji oh-my-posh-bin papirus-icon-theme slurp ttf-hack-nerd wl-clipboard waybar-git wofi
+paru -S --needed ark bluez bluez-utils brightnessctl btop catpuccin-gtk-theme-mocha cava fastfetch fcitx5 fcitx-chinese-addons fish gnome-bluetooth grim hyprcursor-dracula-kde-git hypridle hyprland hyprlock hyprpaper hyprpolkitagent hyprsunset kitty libinput-gestures ly mako nemo nautilus noto-fonts-cjk noto-fonts-emoji oh-my-posh-bin papirus-icon-theme slurp ttf-hack-nerd wl-clipboard waybar-cava wofi
 
 # === Hardlink settings files ===
 cd ~/.config/
 rm -rf ./hypr/ ./waybar/ ./wofi/ ./mako/ 2>/dev/null
-mkdir -p ./hypr/ ./waybar/ ./wofi/ ./mako/
+mkdir -p ./hypr/ ./waybar/ ./wofi/ ./mako/ 
 cd ~/Settings/
 ln -f Hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
 ln -f Hyprland/hypridle.conf ~/.config/hypr/hypridle.conf
@@ -28,4 +29,3 @@ ln -f Misc/xwayland-flags ~/.config/spotify-flags.conf
 
 # === Change username ===
 find ~/Settings -type f -print0 | xargs -0 sed -i '' -e "s/aurora/$USER/g"
-

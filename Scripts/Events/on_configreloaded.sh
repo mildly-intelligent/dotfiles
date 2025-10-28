e6f385fb424df95cfd2137a1eedb12fb639d2284
Aurora Daniel <noahd29@parkschool.net>
2025-10-28 17:17:18 -0400

    Fixed stupidity

---
+++ Scripts/Events/on_configreloaded.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pkill waybar
+waybar --config /home/aurora/Settings/Waybar/Config.jsonc --style /home/aurora/Settings/Waybar/Style.css &
+
+rm ~/Pictures/Wallpapers/.png 2>/dev/null
+ln -s ~/Pictures/Wallpapers/*. ~/Pictures/Wallpapers/.png
+
+hyprctl hyprpaper reload ,"~/Pictures/Wallpapers/.png"
+
+libinput-gestures-setup restart
+
+makoctl reload
+
+swww img ~/Settings/Wallpapers/$(hyprctl activeworkspace -j | jq .id) -t wave

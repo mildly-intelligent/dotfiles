d730db08cde50f177be679d39415392cc441b2b0
Aurora Daniel <noahd29@parkschool.net>
2025-11-06 13:25:14 -0500

    Reload * Added different notification if wayland fails

--- Scripts/Events/on_configreloaded.sh
+++ Scripts/Events/on_configreloaded.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-hyprctl notify 1 3000 "rgb(f9e2af)" "Reloading"
+hyprctl -q notify 1 3000 "rgb(f9e2af)" "Reloading"
 
 pkill waybar
 waybar --config /home/aurora/Settings/Waybar/Config.jsonc --style /home/aurora/Settings/Waybar/Style.css &
-hyprctl notify 5 3000 "rgb(a6e3a1)" "Waybar reloaded"
+sleep 1
+echo $(ps -U aurora | grep $!)
+
+if [[ "$(ps -U aurora | grep $!)" != "" ]]; then
+	hyprctl -q notify 5 3000 "rgb(a6e3a1)" "Waybar reloaded"
+else
+	hyprctl -q notify 3 3000 "rgb(a6e3a1)" "Waybar failed to load"
+fi
 
 rm ~/Pictures/Wallpapers/.png 2>/dev/null
 ln -s ~/Pictures/Wallpapers/*. ~/Pictures/Wallpapers/.png
 
 libinput-gestures-setup restart
-hyprctl notify 5 3000 "rgb(a6e3a1)" "libinput-gestures reloaded"
+hyprctl -q notify 5 3000 "rgb(a6e3a1)" "libinput-gestures reloaded"
 
 makoctl reload
-hyprctl notify 5 3000 "rgb(a6e3a1)" "Mako reloaded"
+hyprctl -q notify 5 3000 "rgb(a6e3a1)" "Mako reloaded"
 
-swww img ~/Settings/Wallpapers/$(hyprctl activeworkspace -j | jq .id) -t wave
-hyprctl notify 5 3000 "rgb(a6e3a1)" "Swww reloaded"
+swww img ~/Settings/Wallpapers/$(hyprctl -q activeworkspace -j | jq .id) -t wave
+hyprctl -q notify 5 3000 "rgb(a6e3a1)" "Swww reloaded"
 
 
-hyprctl notify 5 3000 "rgb(a6e3a1)" "Fully reloaded"
+hyprctl -q notify 5 3000 "rgb(a6e3a1)" "Fully reloaded"
 
